0428c87c5ba3de4fd131cc0da1f4d6841be21bb4
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2019-02-18 12:34:02 -0800

    Updates

---
+++ precache-manifest.f18376bab64b565f640e7bb459e8ddb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c42b12bc7cd8d50869c",
+    "url": "/static/css/main.44c540e9.chunk.css"
+  },
+  {
+    "revision": "4c42b12bc7cd8d50869c",
+    "url": "/static/js/main.4c42b12b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f39624b01d29e1627497",
+    "url": "/static/js/2.f39624b0.chunk.js"
+  },
+  {
+    "revision": "584ef04b43e28373caaa3d04136ed196",
+    "url": "/index.html"
+  }
+];
